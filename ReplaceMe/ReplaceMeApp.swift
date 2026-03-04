d62d5cc842dc4c3fc4c1c364a2c4221637cc3edf
kadirkaragoz <karagozkadir@hotmail.com>
2026-03-04 17:56:31 +0300

    Initial Commit

---
+++ ReplaceMe/ReplaceMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplaceMeApp.swift
+//  ReplaceMe
+//
+//  Created by kadirkaragoz on 4.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReplaceMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
